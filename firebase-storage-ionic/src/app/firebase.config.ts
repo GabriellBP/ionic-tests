cb3963d69234d83bf24d229203848bcc6daa230d
GabriellBP <gbp@ic.ufal.br>
2019-01-12 17:28:25 -0300

    Storing images from the native camera to firebase store

---
+++ firebase-storage-ionic/src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  databaseURL: "your-database-url",
+  projectId: "yout-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id"
+};
