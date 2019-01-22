69f791c9baf4f2259a8492a34a9fea252faae6d1
GabriellBP <gbp@ic.ufal.br>
2019-01-22 15:44:15 -0300

    Installation and configuration of the google maps plugin

--- maps/src/pages/home/home.ts
+++ maps/src/pages/home/home.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
+import {
+  LocationService,
+  MyLocation,
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-home',
@@ -8,6 +20,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+  }
 
   }
 
