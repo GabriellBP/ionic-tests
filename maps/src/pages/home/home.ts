c22f21b58188383390ca7ee2367cb5a27eaa3196
GabriellBP <gbp@ic.ufal.br>
2019-01-22 15:45:40 -0300

    Simple map canvas with button to add a mark in the actual position

--- maps/src/pages/home/home.ts
+++ maps/src/pages/home/home.ts
@@ -18,10 +18,44 @@ import {
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  map: GoogleMap;
   constructor(public navCtrl: NavController) {
   }
 
+  ionViewDidLoad() {
+    this.loadMap();
   }
+
+  loadMap() {
+
+    LocationService.getMyLocation().then((myLocation: MyLocation) => {
+
+      let options: GoogleMapOptions = {
+        camera: {
+          target: myLocation.latLng,
+          zoom: 18,
+          tilt: 30
+        }
+      };
+      this.map = GoogleMaps.create('map_canvas', options);
+    });
+  }
+
+  addMarker() {
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'My location',
+      icon: 'red',
+      animation: 'DROP',
+      position: this.map.getCameraTarget()
+      // position: {
+      //   lat: 43.0741904,
+      //   lng: -89.3809802
+      // }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
+
 
 }
